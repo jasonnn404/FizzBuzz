066d3734ef150835142736b9be49c0023b010402
Jason Li <jasonnn404l@gmail.com>
2025-09-09 12:17:18 -0400

    created Main class

---
+++ src/lab1/Main.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class Main {
+    public static void main() {
+        int j = 1;
+        while (j < 100) {
+            FizzBuzz.doFizzBuzz(j);
+            j++;
+        }
+    }
+}
